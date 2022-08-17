0fd6a556818722771438db1f00572b97bc8a8516
G.A <42187739+Kether180@users.noreply.github.com>
2022-08-17 15:05:18 +0200

    first-commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace charpfibb
+{
+    class Program
+    {
+        public static int fib(int i)
+        {
+
+            if (i <= 2) return 1;
+            return fib(i - 2) + fib(i - 1);
+
+        }
+
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine(fib(20));
+
+
+
+        }
+
+    }
+
+}
